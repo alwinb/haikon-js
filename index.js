64697ccfd17e90eb66f8ee2a601beee46f47a822
Alwin Blok <alwinb@users.noreply.github.com>
2021-04-22 19:48:21 +0200

    Svg serialiser

--- index.js
+++ index.js
@@ -8,20 +8,35 @@ const createElementNS = (ns, tagName) =>
   setProto ({ tagName, attributes:{}, children:[] }, Elem.prototype)
 
 class Elem {
-  append (...children) { this.children.splice (0, 0, ...children) }
+  append (...children) { this.children.splice (Infinity, 0, ...children) }
   setAttribute (k, v) { this.attributes [k] = v }
+  toSVGString () { return toSVGString (this) }
+}
+
+function toSVGString (elem) {
+  return [...renderIt (elem)] .join ('')
+}
+
+function* renderIt ({ tagName, attributes:atts, children }) {
+  yield `<${tagName}`
+  for (const k in atts) yield ` ${k}="${atts[k]}"` // TODO: escapes
+  if (children.length) {
+    yield '>'
+    for (const child of children) yield* renderIt (child)
+    yield `</${tagName}>`
+  }
+  else yield '/>'
+  yield ('\n')
 }
 
 
 // Exports
 // -------
 
-let svg 
+const svg = require ('./scripts/svg')
+assign (svg, svg._renderers (createElementNS))
 
 module.exports = {
   hvif: require ('./scripts/hvif'),
-  svg: (svg = require ('./scripts/svg'))
+  svg
 }
-
-assign (svg, svg._renderers (createElementNS))
-log (module.exports)
--- test/test.js
+++ test/test.js
@@ -6,18 +6,25 @@ const log = console.log.bind (console)
 const { hvif: { parseIcon }, svg: { renderIcon } } = require ('../')
 
 const dir = '../examples/haiku-icons/'
-var sample = 'App_Tracker'
-var sample = 'Device_Harddisk'
-var sample = 'Website_Comment'
-var sample = 'Server_Syslog'
-var sample = 'Trash_Empty'
-var sample = 'Trash_Full'
-var sample = 'Website_Comment'
-var sample = 'App_Icon-O-Matic'
 
-var data = fs.readFileSync (dir + sample)
-var icon = parseIcon (data, sample)
-log (util.inspect (icon, { depth:100 }))
+const samples = [
+  'App_Tracker',
+  'Device_Harddisk',
+  'Website_Comment',
+  'Server_Syslog',
+  'Trash_Empty',
+  'Trash_Full',
+  'Website_Comment',
+  'App_Icon-O-Matic'
+]
 
-var svg = renderIcon (icon)
-log (util.inspect (svg, { depth:100 }))
+// log (util.inspect (icon, { depth:100 }))
+// log (util.inspect (svg, { depth:100 }))
+log ('<style>html { font-size:24px; padding:2rem }.haikon { font-size:100px }</style>')
+for (let sample of samples) {
+  var data = fs.readFileSync (dir + sample)
+  var icon = parseIcon (data, sample)
+  process.stdout.write (renderIcon (icon) .toSVGString ())
+}
+
+process.exit (205)
